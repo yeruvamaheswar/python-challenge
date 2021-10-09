593ef18df8c9095dd16f36c160e46a429e3ce1a1
Uma Maheswar Reddy Yeruva <mr7macbookpro@SM7-MacBook.local>
2021-10-09 18:39:57 -0500

    cleaning Pybank script

--- PyBank/main.py
+++ PyBank/main.py
@@ -32,23 +32,28 @@ with open(CsvPath) as csvfile:
         AllDates.append(row[0])
         AllAmounts.append(int(row[1]))
     
-    
+    #Apending Monthly profits.
     for i in range(len(AllAmounts)-1):
         MonthlyProfitChange.append(AllAmounts[i+1]-AllAmounts[i])       
     
-    
+    #No of months.
     TotalMonths= len(AllDates)
+    #Sum of all amounts.
     TotalPL = sum(AllAmounts)
+    #Avarage Profit change.
     AvarageProfitChange = round(sum(MonthlyProfitChange)/len(MonthlyProfitChange),2)
 
+    #Calculating Maximum Profit cahnge and Maximum profit chnage month.
     MaxProfitChange = max(MonthlyProfitChange)
     MaxProfitChangeIndex = MonthlyProfitChange.index(MaxProfitChange)+1
     MaxProfitMonth = AllDates[MaxProfitChangeIndex]
 
+    #Calculating Minimum Profit cahnge and Minimum profit chnage month.
     MinProfitChange = min(MonthlyProfitChange)
     MinProfitChangeIndex = MonthlyProfitChange.index(MinProfitChange)+1
     MinProfitMonth = AllDates[MinProfitChangeIndex]
 
+    #Printing all the Results on Terminal.
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {TotalMonths}")
@@ -56,10 +61,12 @@ with open(CsvPath) as csvfile:
     print (f"Average Change: ${AvarageProfitChange}")
     print(f"Greatest Increase in Profits: {MaxProfitMonth} (${MaxProfitChange})")
     print(f"Greatest Decrease in Profits: {MinProfitMonth} (${MinProfitChange})")
-    
-OutputPath= "/Users/mr7macbookpro/Documents/DAV BC/HomeWork/3. PythonChallenge/python-challenge/PyBank/analysis"
 
+# Path where the Output CSV file stored.   
+OutputPath= "/Users/mr7macbookpro/Documents/DAV BC/HomeWork/3. PythonChallenge/python-challenge/PyBank/analysis"
+# Joining the path to get the full path of csv file.
 AnalysisTextFile = os.path.join(OutputPath,"FinancialAnalysisReport.txt")
+#Printing all the results on to Text file.
 with open(AnalysisTextFile,"w") as file:
     file.write("Financial Analysis")
     file.write("\n")
